90aad5286d241ca23c9610f877e61fda0c615aac
Tom Wilson <tom@Yuxa.local>
2025-11-13 21:23:27 -0500

    feat: Implement basic shell in 10_ns

--- userspace/planten_coreutils/src/bin/10_ns.rs
+++ userspace/planten_coreutils/src/bin/10_ns.rs
@@ -1,6 +1,21 @@
-use std::env;
+
+use std::io::{self, Write};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("10_ns: {:?}", args);
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        if io::stdin().read_line(&mut input).unwrap() == 0 {
+            break;
+        }
+
+        let input = input.trim();
+        if input == "exit" {
+            break;
+        }
+
+        println!("unknown command: {}", input);
+    }
 }
